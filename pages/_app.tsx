84f068c57dcba5dfe7bebd704d40ebbdebd7228c
Kristián Duda <kristian.duda@extropy.sk>
2023-09-22 22:42:24 +0200

    add shadcn, theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import { SessionProvider } from "next-auth/react";
 import "./styles.css";
 
 import type { Session } from "next-auth";
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { trpc } from "../utils/trpc";
+import { trpc } from "utils/trpc";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -13,7 +14,14 @@ function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
