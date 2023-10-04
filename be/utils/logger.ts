9eee82d1beaaa5084c17bdcce69c67b26eba28c3
Kristián Duda <kristian.duda@extropy.sk>
2023-10-04 11:39:42 +0200

    fix logger

--- be/utils/logger.ts
+++ be/utils/logger.ts
@@ -1,32 +1,31 @@
 import pino from "pino";
-import { LokiOptions } from "pino-loki";
+import loki from "pino-loki";
+import pretty from "pino-pretty";
 
 const buildStream = () => {
   if (process.env.LOKI_HOST && process.env.LOKI_CREDENTIALS) {
     const credentials = process.env.LOKI_CREDENTIALS.split(":");
     if (credentials.length === 2) {
-      return pino.transport<LokiOptions>({
-        target: "pino-loki",
-        options: {
-          batching: true,
-          interval: 5,
+      return loki({
+        batching: true,
+        interval: 5,
 
-          host: process.env.LOKI_HOST,
-          labels: { app: "club", env: process.env.NODE_ENV },
-          basicAuth: {
-            username: credentials[0],
-            password: credentials[1],
-          },
+        host: process.env.LOKI_HOST,
+        labels: {
+          app: "club",
+          env: process.env.NODE_ENV,
+          version: process.env.VERCEL_GITHUB_COMMIT_SHA!,
+        },
+        basicAuth: {
+          username: credentials[0],
+          password: credentials[1],
         },
       });
     }
   }
 
-  return pino.transport({
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
+  return pretty({
+    colorize: true,
   });
 };
 
